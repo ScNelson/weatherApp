5890fd01f44a955a68f6264045fd1531a5e152e8
Steven Nelson <stevenNelson@Stevens-MacBook-Pro-7.local>
2020-11-07 18:09:17 -0500

    Update GetMapping, add PostMapping

--- src/main/java/com/tts/weatherapp/controllers/WeatherController.java
+++ src/main/java/com/tts/weatherapp/controllers/WeatherController.java
@@ -1,5 +1,6 @@
 package com.tts.weatherapp.controllers;
 
+import com.tts.weatherapp.models.Request;
 import com.tts.weatherapp.models.Response;
 import com.tts.weatherapp.service.WeatherService;
 
@@ -7,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class WeatherController {
@@ -15,8 +17,14 @@ public class WeatherController {
 
     @GetMapping
     public String getIndex(Model model) {
-        Response response = weatherService.getForecast("43210");
-        model.addAttribute("data", response);
+        model.addAttribute("request", new Request());
+        return "index";
+    }
+
+    @PostMapping
+    public String postIndex(Request request, Model model) {
+        Response data = weatherService.getForecast(request.getZipCode());
+        model.addAttribute("data", data);
         return "index";
     }
 }
