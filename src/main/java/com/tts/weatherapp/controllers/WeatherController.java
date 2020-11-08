74491462a0b02fdcb1dd461f3ad901b2512dce36
Steven Nelson <stevenNelson@Stevens-MacBook-Pro-7.local>
2020-11-08 02:07:37 -0500

    Added repo, fill model w/10 most recent requests

--- src/main/java/com/tts/weatherapp/controllers/WeatherController.java
+++ src/main/java/com/tts/weatherapp/controllers/WeatherController.java
@@ -2,6 +2,7 @@ package com.tts.weatherapp.controllers;
 
 import com.tts.weatherapp.models.Request;
 import com.tts.weatherapp.models.Response;
+import com.tts.weatherapp.repository.WeatherRepository;
 import com.tts.weatherapp.service.WeatherService;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +16,9 @@ public class WeatherController {
     @Autowired
     private WeatherService weatherService;
 
+    @Autowired
+    private WeatherRepository weatherRepository;
+
     @GetMapping
     public String getIndex(Model model) {
         model.addAttribute("request", new Request());
@@ -25,6 +29,8 @@ public class WeatherController {
     public String postIndex(Request request, Model model) {
         Response data = weatherService.getForecast(request.getZipCode());
         model.addAttribute("data", data);
+        Iterable<Request> zipCodes = weatherRepository.findFirst10ByOrderByIdDesc();
+        model.addAttribute("zipCodes", zipCodes);
         return "index";
     }
 }
