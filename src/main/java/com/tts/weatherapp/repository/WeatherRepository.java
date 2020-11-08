5454afde8d03474248c0ddd74caf1a361d6f46ed
Steven Nelson <stevenNelson@Stevens-MacBook-Pro-7.local>
2020-11-08 02:08:30 -0500

    Added method for 10 most recent requests

--- src/main/java/com/tts/weatherapp/repository/WeatherRepository.java
+++ src/main/java/com/tts/weatherapp/repository/WeatherRepository.java
@@ -1,9 +1,13 @@
 package com.tts.weatherapp.repository;
 
+import java.util.List;
+
 import com.tts.weatherapp.models.Request;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface WeatherRepository extends CrudRepository<Request, Long> {
-    
+    List<Request> findFirst10ByOrderByIdDesc();
 }
