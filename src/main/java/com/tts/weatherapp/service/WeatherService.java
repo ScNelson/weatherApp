508f2b11752071083e46b52115a14239de94545c
Steven Nelson <stevenNelson@Stevens-MacBook-Pro-7.local>
2020-11-08 02:08:45 -0500

    Add method for saving to repo

--- src/main/java/com/tts/weatherapp/service/WeatherService.java
+++ src/main/java/com/tts/weatherapp/service/WeatherService.java
@@ -1,7 +1,10 @@
 package com.tts.weatherapp.service;
 
+import com.tts.weatherapp.models.Request;
 import com.tts.weatherapp.models.Response;
+import com.tts.weatherapp.repository.WeatherRepository;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
@@ -12,11 +15,16 @@ public class WeatherService {
     @Value("${api_key}")
     private String apiKey;
 
+    @Autowired
+    private WeatherRepository weatherRepository;
+
     public Response getForecast(String zipCode) {
         String url = "http://api.openweathermap.org/data/2.5/weather?zip=" + 
             zipCode + "&units=imperial&appid=" + apiKey;
         RestTemplate restTemplate = new RestTemplate();
         try {
+            Request request = new Request(zipCode);
+            weatherRepository.save(request);
             return restTemplate.getForObject(url, Response.class);
         } catch (HttpClientErrorException exception) {
             Response response = new Response();
