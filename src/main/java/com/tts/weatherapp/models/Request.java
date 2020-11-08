515c4d881feb85a327bfd99757e37adfe4b60220
Steven Nelson <stevenNelson@Stevens-MacBook-Pro-7.local>
2020-11-08 02:08:15 -0500

    Added id for Requests (to make work w/repo)

--- src/main/java/com/tts/weatherapp/models/Request.java
+++ src/main/java/com/tts/weatherapp/models/Request.java
@@ -1,10 +1,25 @@
 package com.tts.weatherapp.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Request {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+     
     private String zipCode;
 
 
     public Request() {}
+
+    public Request(String zipCode) {
+        this.zipCode = zipCode;
+    }    
 
     public String getZipCode() {
         return this.zipCode;
